c8c96b13cac7348d8b88f2daf6e8181154de2f19
Aimuel Emmanuel <aimuelemmanuel@gmail.com>
2023-05-11 12:23:11 +0100

    second commit

--- src/components/Crew.js
+++ src/components/Crew.js
@@ -20,18 +20,7 @@ const CrewCommander = () => {
         {step === 0 && (
           <section className="text-center lg:text-left mt-16 flex flex-col-reverse  lg:flex-row justify-between items-center lg:items-start">
             <div className="mt-12 lg:mt-0">
-              <p className="lg:text-2xl text-xl bellefair">COMMANDER</p>
-              <p className="text-3xl lg:text-5xl bellefair my-8">
-                DOUGLAS HURLEY
-              </p>
-              <p>
-                Douglas Gerald Hurley is an American engineer, former{" "}
-                <br className="hidden md:block" /> Marine Corps pilot and former
-                NASA astronaut. He <br className="hidden md:block" /> launched
-                into space for the third time as commander of{" "}
-                <br className="hidden md:block" /> Crew Dragon Demo-2.
-              </p>
-              <div className="flex space-x-5 mt-[6rem] justify-center lg:justify-normal mb-10 lg:mb-0">
+              <div className="flex space-x-5 justify-center lg:justify-normal mb-10">
                 <div
                   onClick={() => {
                     setStep(0);
@@ -68,6 +57,17 @@ const CrewCommander = () => {
                   &nbsp;
                 </div>
               </div>
+              <p className="lg:text-2xl text-xl bellefair">COMMANDER</p>
+              <p className="text-3xl lg:text-5xl bellefair my-8">
+                DOUGLAS HURLEY
+              </p>
+              <p className="mb-10 mb:hidden">
+                Douglas Gerald Hurley is an American engineer, former{" "}
+                <br className="hidden md:block" /> Marine Corps pilot and former
+                NASA astronaut. He <br className="hidden md:block" /> launched
+                into space for the third time as commander of{" "}
+                <br className="hidden md:block" /> Crew Dragon Demo-2.
+              </p>
             </div>
             <hr className="lg:hidden w-[19rem]" />
             <div className="lg:mr-10">
@@ -83,21 +83,7 @@ const CrewCommander = () => {
         {step === 1 && (
           <section className="text-center lg:text-left mt-16 flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start">
             <div className="mt-12 lg:mt-0">
-              <p className="lg:text-2xl text-xl bellefair">
-                MISSION SPECIALIST
-              </p>
-              <p className="text-3xl lg:text-5xl bellefair my-8">
-                MARK SHUTTLEWORTH
-              </p>
-              <p>
-                Mark Richard Shuttleworth is the founder and CEO of{" "}
-                <br className="hidden md:block" /> Canonical, the company behind
-                the Linux-based Ubuntu <br className="hidden md:block" />{" "}
-                operating system. Shuttleworth became the first South{" "}
-                <br className="hidden md:block" /> African to travel to space as
-                a space tourist.
-              </p>
-              <div className="flex space-x-5 mt-[6rem] justify-center lg:justify-normal mb-10 lg:mb-0">
+              <div className="flex space-x-5 justify-center lg:justify-normal mb-10">
                 <div
                   onClick={() => {
                     setStep(0);
@@ -134,6 +120,20 @@ const CrewCommander = () => {
                   &nbsp;
                 </div>
               </div>
+              <p className="lg:text-2xl text-xl bellefair">
+                MISSION SPECIALIST
+              </p>
+              <p className="text-3xl lg:text-5xl bellefair my-8">
+                MARK SHUTTLEWORTH
+              </p>
+              <p className="mb-10 mb:hidden">
+                Mark Richard Shuttleworth is the founder and CEO of{" "}
+                <br className="hidden md:block" /> Canonical, the company behind
+                the Linux-based Ubuntu <br className="hidden md:block" />{" "}
+                operating system. Shuttleworth became the first South{" "}
+                <br className="hidden md:block" /> African to travel to space as
+                a space tourist.
+              </p>
             </div>
             <hr className="lg:hidden w-[19rem]" />
             <div className="mr-10">
@@ -149,20 +149,7 @@ const CrewCommander = () => {
         {step === 2 && (
           <section className="text-center lg:text-left mt-16 flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start">
             <div className="mt-12 lg:mt-0">
-              <p className="lg:text-2xl text-xl bellefair">PILOT</p>
-              <p className="text-3xl lg:text-5xl bellefair my-8">
-                VICTOR GLOVER
-              </p>
-              <p>
-                Pilot on the first operational flight of the SpaceX Crew Dragon{" "}
-                <br className="hidden md:block" /> to the International Space
-                Station. Glover is a commander in the{" "}
-                <br className="hidden md:block" /> U.S. Navy where he pilots an
-                F/A-18. He was a crew member <br className="hidden md:block" />{" "}
-                of Expedition 64, and served as a station systems flight
-                engineer.
-              </p>
-              <div className="flex space-x-5 mt-[6rem] justify-center lg:justify-normal mb-10 lg:mb-0">
+              <div className="flex space-x-5 justify-center lg:justify-normal mb-10">
                 <div
                   onClick={() => {
                     setStep(0);
@@ -199,6 +186,19 @@ const CrewCommander = () => {
                   &nbsp;
                 </div>
               </div>
+              <p className="lg:text-2xl text-xl bellefair">PILOT</p>
+              <p className="text-3xl lg:text-5xl bellefair my-8">
+                VICTOR GLOVER
+              </p>
+              <p className="mb-10 mb:hidden">
+                Pilot on the first operational flight of the SpaceX Crew Dragon{" "}
+                <br className="hidden md:block" /> to the International Space
+                Station. Glover is a commander in the{" "}
+                <br className="hidden md:block" /> U.S. Navy where he pilots an
+                F/A-18. He was a crew member <br className="hidden md:block" />{" "}
+                of Expedition 64, and served as a station systems flight
+                engineer.
+              </p>
             </div>
             <hr className="lg:hidden w-[19rem]" />
             <div className="mr-10">
@@ -214,20 +214,7 @@ const CrewCommander = () => {
         {step === 3 && (
           <section className="text-center lg:text-left mt-16 flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start">
             <div className="mt-12 lg:mt-0">
-              <p className="lg:text-2xl text-xl bellefair">FLIGHT ENGINEER</p>
-              <p className="text-3xl lg:text-5xl bellefair my-8">
-                ANOUSHEH ANSARI
-              </p>
-              <p>
-                Anousheh Ansari Anousheh Ansari is an Iranian American engineer
-                and <br className="hidden md:block" /> co-founder of Prodea
-                Systems. Ansari was the fourth{" "}
-                <br className="hidden md:block" /> self-funded space tourist,
-                the first self-funded woman to{" "}
-                <br className="hidden md:block" /> fly to the ISS, and the first
-                Iranian in space.
-              </p>
-              <div className="flex space-x-5 mt-[6rem] justify-center lg:justify-normal mb-10 lg:mb-0">
+              <div className="flex space-x-5  justify-center lg:justify-normal mb-10">
                 <div
                   onClick={() => {
                     setStep(0);
@@ -264,6 +251,19 @@ const CrewCommander = () => {
                   &nbsp;
                 </div>
               </div>
+              <p className="lg:text-2xl text-xl bellefair">FLIGHT ENGINEER</p>
+              <p className="text-3xl lg:text-5xl bellefair my-8">
+                ANOUSHEH ANSARI
+              </p>
+              <p className="mb-10 mb:hidden">
+                Anousheh Ansari Anousheh Ansari is an Iranian American engineer
+                and <br className="hidden md:block" /> co-founder of Prodea
+                Systems. Ansari was the fourth{" "}
+                <br className="hidden md:block" /> self-funded space tourist,
+                the first self-funded woman to{" "}
+                <br className="hidden md:block" /> fly to the ISS, and the first
+                Iranian in space.
+              </p>
             </div>
             <hr className="lg:hidden w-[21rem]" />
 
